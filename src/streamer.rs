f11e984a9b82408680c841a06c1fb0f38c119ca8
isaactsg <idw.wismer@gmail.com>
2018-11-21 21:00:19 -0500

    Fix participant printing

--- src/streamer.rs
+++ src/streamer.rs
@@ -27,7 +27,7 @@ use encoding::{DecoderTrap, Encoding};
 use std::collections::HashMap;
 use std::error::Error;
 use std::fs::File;
-use std::io::{Read, Write};
+use std::io::{self, Read, Write};
 use std::net::Ipv4Addr;
 use std::net::TcpListener;
 use std::net::TcpStream;
@@ -97,7 +97,7 @@ fn get_bibchip(file_path: String) -> HashMap<i32, String> {
 
 fn get_participants(
     ppl_path: String,
-    bib_chip_map: HashMap<i32, String>,
+    bib_chip_map: &HashMap<i32, String>,
 ) -> HashMap<String, participant::Participant> {
     let ppl = match read_file_utf8(ppl_path.clone()) {
         Err(_desc) => match read_file_1252(ppl_path.clone()) {
@@ -216,19 +216,29 @@ fn main() {
                 .validator(is_file)
                 .requires("bibchip"),
         )
+        .arg(
+            Arg::with_name("is_unbuffered")
+                .help("Unbuffer the output output")
+                .short("u")
+                .long("unbuffer")
+                .takes_value(false),
+        )
         .get_matches();
 
     let mut bib_chip_map: Option<HashMap<i32, String>> = None;
     let mut participants: Option<HashMap<String, participant::Participant>> = None;
     if matches.is_present("bibchip") {
         let path = matches.value_of("bibchip").unwrap();
-        bib_chip_map = Some(get_bibchip(path.to_string()));
+        let bib_chips = get_bibchip(path.to_string());
         // println!("{:?}", bib_chip_map);
         if matches.is_present("participants") {
             let path = matches.value_of("participants").unwrap();
-            participants = Some(get_participants(path.to_string(), bib_chip_map.unwrap()));
+            participants = Some(get_participants(path.to_string(), &bib_chips));
         }
+        bib_chip_map = Some(bib_chips);
     }
+
+    let is_unbuffered = matches.is_present("is_unbuffered");
 
     // Check if the user has specified to save the reads to a file
     let mut file_writer: Option<File> = None;
@@ -366,25 +376,44 @@ fn main() {
                 match chip_read::ChipRead::new(read.to_string()) {
                     Err(desc) => println!("Error reading chip {}", desc),
                     Ok(read) => {
-                        if participants.is_some() {
-                            let participants_unwrap = participants.as_mut().unwrap();
-                            let last_participant = match participants_unwrap.get(&read.tag_id) {
-                                Some(participant) => participant.clone(),
-                                None => participants_unwrap.get(&"unkown".to_string()).unwrap(),
-                            };
-                            print!(
-                                "Total Reads: {} Last Read: {} {} {} {}\r",
-                                read_count,
-                                last_participant.bib,
-                                last_participant.first_name,
-                                last_participant.last_name,
-                                read.timestamp
-                            );
-                        } else {
-                            print!(
+                        // let bib_chip_map = bib_chip_map.clone();
+                        match &participants {
+                            Some(participants_map) => match participants_map.get(&read.tag_id) {
+                                Some(participant) => {
+                                    print!(
+                                        "Total Reads: {} Last Read: {} {} {} {}\r",
+                                        read_count,
+                                        participant.bib,
+                                        participant.first_name,
+                                        participant.last_name,
+                                        read.timestamp
+                                    );
+                                }
+                                None => match &bib_chip_map {
+                                    Some(bib_chip_map) => {
+                                        let mut unknown_bib = 0;
+                                        for (bib, chip) in bib_chip_map.iter() {
+                                            if *chip == read.tag_id {
+                                                unknown_bib = bib.clone();
+                                                break;
+                                            }
+                                        }
+                                        print!("Total Reads: {} Last Read: {} Unknown Participant {}\r",
+                                                    read_count,
+                                                    unknown_bib,
+                                                    read.timestamp
+                                                );
+                                    }
+                                    None => (),
+                                },
+                            },
+                            None => print!(
                                 "Total Reads: {} Last Read: {} {}\r",
                                 read_count, read.tag_id, read.timestamp
-                            );
+                            ),
+                        };
+                        if is_unbuffered {
+                            io::stdout().flush().unwrap();
                         }
                     }
                 };
