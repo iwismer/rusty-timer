b3329473f066442f500b31946a7d7e4cb7a053ba
Isaac Wismer <isaac.wismer@cascination.com>
2020-04-23 10:36:14 +0200

    Add tests

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -34,7 +34,7 @@ pub fn is_port(port: String) -> Result<(), String> {
 }
 
 /// Check that the path does not already point to a file
-pub fn is_path(path_str: String) -> Result<(), String> {
+pub fn is_empty_path(path_str: String) -> Result<(), String> {
     let path = Path::new(&path_str);
     match path.exists() {
         true => Err("File exists on file system! Use a different file".to_string()),
@@ -42,10 +42,10 @@ pub fn is_path(path_str: String) -> Result<(), String> {
     }
 }
 
-/// Check that the path does not already point to a file
+/// Check that the file exists
 pub fn is_file(file_str: String) -> Result<(), String> {
     let path = Path::new(&file_str);
-    match path.exists() {
+    match path.is_file() {
         true => Ok(()),
         false => Err("File doesn't exists on file system! Use a different file".to_string()),
     }
@@ -55,5 +55,88 @@ pub fn is_delay(delay: String) -> Result<(), String> {
     match delay.parse::<u32>() {
         Ok(_) => Ok(()),
         Err(_) => Err("Invalid delay value".to_string()),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_is_ip_addr() {
+        assert!(is_ip_addr("1.1.1.1".to_string()).is_ok());
+        assert!(is_ip_addr("192.168.1.1".to_string()).is_ok());
+        assert!(is_ip_addr("255.255.255.255".to_string()).is_ok());
+        assert!(is_ip_addr("0.0.0.0".to_string()).is_ok());
+        assert!(is_ip_addr("10.0.0.1".to_string()).is_ok());
+
+        assert!(is_ip_addr("-1.-1.-.-1".to_string()).is_err());
+        assert!(is_ip_addr("foobar".to_string()).is_err());
+        assert!(is_ip_addr("1.1.1".to_string()).is_err());
+        assert!(is_ip_addr("1.1.1.1.1".to_string()).is_err());
+        assert!(is_ip_addr("1.1.1.1:8080".to_string()).is_err());
+        assert!(is_ip_addr("".to_string()).is_err());
+    }
+
+    #[test]
+    fn test_is_socket_addr() {
+        assert!(is_socket_addr("1.1.1.1:1".to_string()).is_ok());
+        assert!(is_socket_addr("192.168.1.1:8080".to_string()).is_ok());
+        assert!(is_socket_addr("255.255.255.255:60000".to_string()).is_ok());
+        assert!(is_socket_addr("0.0.0.0:0".to_string()).is_ok());
+        assert!(is_socket_addr("10.0.0.1:10000".to_string()).is_ok());
+
+        assert!(is_socket_addr("1.1.1.1".to_string()).is_err());
+        assert!(is_socket_addr("foobar".to_string()).is_err());
+        assert!(is_socket_addr("1.1.1".to_string()).is_err());
+        assert!(is_socket_addr("1.1.1.1.1".to_string()).is_err());
+        assert!(is_socket_addr("1.1.1.1:-1".to_string()).is_err());
+        assert!(is_socket_addr("1.1.1.1:100000000".to_string()).is_err());
+        assert!(is_socket_addr("".to_string()).is_err());
+    }
+
+    #[test]
+    fn test_is_port() {
+        assert!(is_port("1".to_string()).is_ok());
+        assert!(is_port("8080".to_string()).is_ok());
+        assert!(is_port("60000".to_string()).is_ok());
+        assert!(is_port("0".to_string()).is_ok());
+        assert!(is_port("10000".to_string()).is_ok());
+
+        assert!(is_port("-1".to_string()).is_err());
+        assert!(is_port("foobar".to_string()).is_err());
+        assert!(is_port("100000000".to_string()).is_err());
+        assert!(is_port("".to_string()).is_err());
+    }
+
+    #[test]
+    fn test_is_empty_path() {
+        assert!(is_empty_path("".to_string()).is_ok());
+        assert!(is_empty_path("qwerty.txt".to_string()).is_ok());
+
+        assert!(is_empty_path("test_assets/bibchip/empty.txt".to_string()).is_err());
+        assert!(is_empty_path("test_assets/bibchip".to_string()).is_err());
+    }
+
+    #[test]
+    fn test_is_file() {
+        assert!(is_file("test_assets/bibchip/single.txt".to_string()).is_ok());
+
+        assert!(is_file("".to_string()).is_err());
+        assert!(is_file("qwety.txt".to_string()).is_err());
+        assert!(is_file("test_assets/bibchip".to_string()).is_err());
+    }
+
+    #[test]
+    fn test_is_delay() {
+        assert!(is_delay("1".to_string()).is_ok());
+        assert!(is_delay("8080".to_string()).is_ok());
+        assert!(is_delay("60000".to_string()).is_ok());
+        assert!(is_delay("0".to_string()).is_ok());
+        assert!(is_delay("10000".to_string()).is_ok());
+
+        assert!(is_delay("-1".to_string()).is_err());
+        assert!(is_delay("foobar".to_string()).is_err());
+        assert!(is_delay("".to_string()).is_err());
     }
 }
